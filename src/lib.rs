0c9b10f4fbf3e277fc06820f707af6469f74d232
Peter Reid <peter.d.reid@gmail.com>
2016-03-06 07:30:03 -0500

    Fix documentation typo

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-//! ChaCha is a family of 256-bit stream ciphers. This create includes five
+//! ChaCha is a family of 256-bit stream ciphers. This crate includes five
 //! members of the family:
 //!
 //!
