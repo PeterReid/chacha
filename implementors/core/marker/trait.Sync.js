aa3f58774796ae7ff11f270789fbd567ee9970aa
PeterReid <peter.d.reid@gmail.com>
2018-09-13 15:37:12 -0400

    Update documentation

---
+++ implementors/core/marker/trait.Sync.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["byteorder"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"enum\" href=\"byteorder/enum.BigEndian.html\" title=\"enum byteorder::BigEndian\">BigEndian</a>",synthetic:true,types:["byteorder::BigEndian"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"enum\" href=\"byteorder/enum.LittleEndian.html\" title=\"enum byteorder::LittleEndian\">LittleEndian</a>",synthetic:true,types:["byteorder::LittleEndian"]},];
+implementors["chacha"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"chacha/struct.ChaCha.html\" title=\"struct chacha::ChaCha\">ChaCha</a>",synthetic:true,types:["chacha::ChaCha"]},];
+implementors["keystream"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"enum\" href=\"keystream/enum.Error.html\" title=\"enum keystream::Error\">Error</a>",synthetic:true,types:["keystream::Error"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
